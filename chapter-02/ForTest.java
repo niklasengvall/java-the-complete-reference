38cbec3a2ae5e85d1eddc10ee148da43e9d39585
Niklas Engvall <niklas_engvall@hotmail.com>
2020-07-24 01:33:28 +0200

    Added source code with comments for ch. 2, p. 33

--- chapter-02/ForTest.java
+++ chapter-02/ForTest.java
@@ -1,14 +1,35 @@
-/**
- * C02 L04
- * 
- * Demonstrate the for loop. Call this file "ForTest.java".
- */
-public class ForTest {
+// Code examples from:
+//
+// Java The Complete Reference Eleventh Edition
+// Comprehensive Coverage of the Java Language
+// Author: Herbert Schildt
+//
+// Rewritten and tested with comments by Niklas Engvall
+//
+// Chapter: 2 - An overview of Java
+// Page:    33
+// Title:   The If Statement
+// File:    ForTest.java
 
+/*
+    Demonstrate the for loop. 
+    Call this file "ForTest.java".
+*/
+class ForTest {
     public static void main(String[] args) {
-        int x;
+        // int x;  I removed this line, you can declare and assign a value 
+        //         to a variable directly inside the for loop, se below
 
-        for (x = 0; x < 10; x = x + 1)
+        // Old for loop 
+        // for (x = 0; x < 10; x = x + 1)
+        //     System.out.println("This is x: " + x);
+        
+        // For loops four sections:
+        // Initialization; Declare and assign a start value to a variable
+        // Condition; in this case run the loop as long as x is lesser than 10
+        // Itteration; in this case increase x value by 1
+        // Indented statement or block of code to run several times in this case print value of x
+        for (int x = 0; x < 10; x++)
             System.out.println("This is x: " + x);
     }
 }
