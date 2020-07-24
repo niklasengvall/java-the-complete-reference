cde80f06d478f6e5ce48199047d13710e9534508
Niklas Engvall <niklas_engvall@hotmail.com>
2020-07-24 02:56:06 +0200

    Added source code with comments for ch. 3, p. 41

--- chapter-03/Light.java
+++ chapter-03/Light.java
@@ -1,26 +1,45 @@
-/**
- * C03 L01
- * 
- * Compute distance light travels using long variables.
- */
-public class Light {
+// Code examples from:
+//
+// Java The Complete Reference Eleventh Edition
+// Comprehensive Coverage of the Java Language
+// Author: Herbert Schildt
+//
+// Rewritten and tested with comments by Niklas Engvall
+//
+// Chapter: 3 - Data Types, Variables, and Arrays
+// Page:    41
+// Title:   long
+// File:    Light.java
 
-    public static void main(String[] args) {
-        int lightspeed;
-        long days;
-        long seconds;
-        long distance;
 
-        lightspeed = 186000;
+// Compute distance light travels using long variables.
+// I changed the file from original as you can see below and when compiled
+// the class file (bytecode) is smaller compared to the compiled original.
+class Light {
+    public static void main(String args[]) {
+    /* Removed this original lines so you see how you can shrink the code
+    int lightspeed;
+    long days;
+    long seconds;
+    long distance;
+    */
+        // approximate speed of light in miles/second and m/s
+        int lightspeed = 186000, ls_metric = 299792458; // added metric value
+        // specify days here, leave the rest it computes no of seconds and the distance
+        long days = 1000, seconds = days * 24 * 60 * 60, distance = lightspeed * seconds, distance_metric = ls_metric * seconds;
 
-        days = 1000;
-
-        seconds = days * 24 * 60 * 60;
-
-        distance = lightspeed * seconds;
-
+        // lightspeed = 186000; removed, declared and assigned above
+        // days = 1000; removed, declared and assigned above
+        // seconds = days * 24 * 60 * 60; removed, declared and assigned above
+        // distance = lightspeed * seconds; removed, declared and assigned above
+        
+        /* Removed this original lines and shrinked the code so it's more readable
         System.out.print("In " + days);
         System.out.print(" days light will travel about ");
         System.out.println(distance + " miles.");
+        */
+
+        System.out.println("In " + days + " days light will travel about " + distance + " miles.");
+        System.out.println("In " + days + " days light will travel about " + distance_metric + " meters.");
     }
 }
